8f6917a3d14be1faff839190038b35e063ba69aa
Dac Nguyen <dachuu25@gmail.com>
2018-06-04 16:41:14 +0700

    change file name and prepare data

---
+++ application/Library/Constant/GeneralConstant.py
@@ -0,0 +1,6 @@
+def ORIGINAL_DATA_PATH():
+    return "..\\Library\\Data\\OriginalData"
+
+
+def PROCESSED_DATA_PATH():
+    return "..\\Library\\Data\\ProcessedData"
--- application/Tools/ChangeFolderFileName.py
+++ application/Tools/ChangeFolderFileName.py
@@ -1,5 +1,6 @@
 import glob
 import os
+import application.Library.Constant.GeneralConstant as GeneralConstant
 
 
 def changeNameOfFileBaseOnFolder(path):
@@ -17,22 +18,32 @@ def changeNameOfFileBaseOnFolder(path):
                 name += '_TG'
             elif os.path.basename(file_path) == 'ThoiSu':
                 name += '_TS'
-            elif os.path.basename(file_path) != 'Data':
+            elif (os.path.basename(file_path) != 'ProcessedData') & (os.path.basename(file_path) != 'OriginalData'):
                 name += os.path.basename(file_path)
 
             # Reset id
             if curr_id != 0:
                 curr_id = 0
 
+            print('Processing folder {0}: '.format(os.path.basename(file_path)))
+
             # Go Deeper inside folder
             for child_path in glob.glob(file_path + '/*'):
                 changeNameOfFileBaseOnFolderRecursive(child_path, name, curr_id)
                 curr_id += 1
+
         else:  # is file
             print(name + '_' + str(curr_id) + '.txt')
-            os.rename(file_path, name + '_' + str(curr_id) + '.txt')
+            os.rename(file_path, os.path.dirname(file_path) + '\\{0}_{1}.txt'.format(name, str(curr_id)))
 
     changeNameOfFileBaseOnFolderRecursive(path.pop(), '', 0)
 
 
-changeNameOfFileBaseOnFolder(glob.glob("../Data"))
+def run():
+    # processed_data_path = GeneralConstant.PROCESSED_DATA_PATH()
+    # changeNameOfFileBaseOnFolder(glob.glob(processed_data_path))
+    original_data_path = GeneralConstant.ORIGINAL_DATA_PATH()
+    changeNameOfFileBaseOnFolder(glob.glob(original_data_path))
+
+
+run()
